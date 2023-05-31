2920faa6780b577b028b3d9a4d6cf4f1450af5f5
LanderMertensPXL <11801541@student.pxl.be>
2023-05-31 16:46:17 +0000

    Add script personal-info

---
+++ Lab3/personal-info.py
@@ -0,0 +1,5 @@
+FirstName = input("What is your first name? ")
+LastName = input("What is your last name? ")
+Location = input("What is your location? ")
+Age = input("What is your age? ")
+print("Hi "+ FirstName + " " + LastName + " ! Your loacation is " + Location + " and you are " + Age + " years ald.")
