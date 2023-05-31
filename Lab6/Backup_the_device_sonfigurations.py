865b78dc65e12b418b85c08689b34112f595ad3f
LanderMertensPXL <11801541@student.pxl.be>
2023-05-31 16:54:10 +0000

    add script backup the device config

---
+++ Lab6/Backup_the_device_sonfigurations.py
@@ -0,0 +1,26 @@
+from netmiko import ConnectHandler
+import getpass
+
+passwd = getpass.getpass('Please enter the password: ')
+
+cisco_01 = {
+    "device_type": "cisco_ios",
+    "host": "10.125.100.177",
+    "username": "admin",
+    "password": passwd, # Log in password from getpass
+    "secret": passwd # Enable password from getpass
+}
+
+#open file to write command output
+file = open('output.txt', 'w')
+
+connection = ConnectHandler(**cisco_01)
+output1=connection.send_command("show running-config")
+print(output1)
+
+#Write output to file above
+file.write(output1)
+file.close()
+
+print('Closing Connection')
+connection.disconnect()
